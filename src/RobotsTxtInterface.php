0ad07585275a9f4e7af5777e8506a357dce1b721
Baptiste Langlade <baptouuuu@gmail.com>
2017-02-05 12:27:55 +0100

    change return type for RobotsTxtInterface::directives

--- src/RobotsTxtInterface.php
+++ src/RobotsTxtInterface.php
@@ -4,11 +4,16 @@ declare(strict_types = 1);
 namespace Innmind\RobotsTxt;
 
 use Innmind\Url\UrlInterface;
+use Innmind\Immutable\StreamInterface;
 
 interface RobotsTxtInterface
 {
     public function url(): UrlInterface;
-    public function directives(): DirectivesInterface;
+
+    /**
+     * @return StreamInterface<DirectivesInterface>
+     */
+    public function directives(): StreamInterface;
     public function disallows(string $userAgent, UrlInterface $url): bool;
     public function __toString(): string;
 }
